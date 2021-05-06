db55ee488356ab04383255f8fee7ea2b1d54603e
andrearuizg <andreajulianaruiz@hotmail.com>
2021-05-05 23:08:58 -0500

    First commit

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
-docker image rm $(docker images | grep none | awk '{ print $3; }')
-xhost +
-docker build -t segmentar .
+docker build -t data_prediction .
 docker run --rm -it \
     -v ${PWD}/media:/media \
     -v ${PWD}/files:/files \
-    -v ${PWD}/PP:/PP \
     -v ${PWD}/results:/results \
     -v ${PWD}/times:/times \
-    segmentar
+    data_prediction
 
